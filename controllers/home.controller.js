81c2b20c426c53ecdb891f62fbec4f46bfe4bd86
pilander9380bl <32539352+pilander9380bl@users.noreply.github.com>
2017-10-07 12:13:31 +0700

    nodejs server + twig

---
+++ controllers/home.controller.js
@@ -0,0 +1,24 @@
+exports.index = function(req, res) {
+	res.render('home.twig')
+}
+
+
+
+
+
+/*exports.index = function(req, res) {
+	let content = {
+		banner: [
+		{
+			id: 1,
+			url: '//image/img-1.jpg'
+		},
+		{
+			id: 2,
+			url: '//image/img-1.jpg'
+		}
+		]
+	}
+	
+	res.json(content)
+}*/
